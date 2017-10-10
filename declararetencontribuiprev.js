25fa7a8fbf4d1bf36e839dc12f57329d6f8d9f2b
Gustavo Henrique Almeida Militao <gustavoham@BH6F04-FSW.acs.com.br>
2017-10-10 10:55:47 -0300

    Implementações de regras de negócio do botão enviar declaração e abertura da tela.

--- declararetencontribuiprev.js
+++ declararetencontribuiprev.js
@@ -34,7 +34,9 @@
 		$scope.mesFinalRef = "";
 		$scope.mesInicialRefData = null;
 		$scope.mesFInalRefData = null;
-		$scope.empresasPagadoras = [{ razSoc: "TESTE", numIns: "1", valSalarContrib: "0", valRetencInss: "0" }];
+		$scope.empresasPagadoras = [
+			{ razSoc: "TESTE", numIns: "1", valSalarContrib: "0", valRetencInss: "0" }
+		];
 		$scope.anexos = [];
 		$scope.liEAceito = false;
 		$scope.empresaAIncluir = {
@@ -43,7 +45,7 @@
 			valSalarContrib: 0.0,
 			valRetencInss: 0.0
 		}
-		$scope.somaValoresBaseContrib;
+		$scope.somaValoresRetencInss;
 		$scope.filterEmpresa = [];
 		$scope.hideEmpresa = true;
 		$scope.desabilitarCamposEmpresa = false;
@@ -61,28 +63,22 @@
 				razSoc: "EMPRESA DE TESTE DO GUGU1"
 			});
 		}
-		// $scope.dadosRetencContribPrev.listaCNPJ.push({
-		// 	numIns : "43215",
-		// 	razSoc : "EMPRESA DE TESTE DO GUGU2"
-		// });
-		// $scope.dadosRetencContribPrev.listaCNPJ.push({
-		// 	numIns : "54321",
-		// 	razSoc : "EMPRESA DE TESTE DO GUGU3"
-		// });
-		// $scope.dadosRetencContribPrev.listaCNPJ.push({
-		// 	numIns : "32154",
-		// 	razSoc : "EMPRESA DE TESTE DO GUGU4"
-		// });
-		// $scope.dadosRetencContribPrev.listaCNPJ.push({
-		// 	numIns : "21543",
-		// 	razSoc : "EMPRESA DE TESTE DO GUGU5"
-		// });
+
 		$scope.param1 = "Se dados acima incorretos, entrar em contato com sua analista de relacionamento para atualização.";
 		$scope.param2 = "VAI VIR PREENCHIDO {PARAMETRO 2}";
 
 		$scope.adicionarEmpresa = function () {
+			if (!$scope.empresaAIncluir.cnpj || $scope.empresaAIncluir.cnpj == "" ||
+				!$scope.empresaAIncluir.valSalarContrib || $scope.empresaAIncluir.valSalarContrib == 0) {
+					alert("Obrigatório informar o CNPJ e Valor do Salário de Contribuição!");
+			}
+			//Validar CNPJ.
+			if ($scope.empresaAIncluir.valRetencInss > $scope.empresaAIncluir.valSalarContrib) {
+				alert("Valor de retenção do INSS deverá ser inferior ou igual ao valor do salário de contribuição!");
+			}
 			$scope.incluirEmpresa($scope.empresaAIncluir, $scope.empresasPagadoras);
 			$scope.desabilitarCamposEmpresa = false;
+			$scope.somaValoresRetencInss += $scope.empresaAIncluir.valRetencInss;
 			$scope.empresaAIncluir = {
 				numIns: "",
 				razSoc: "",
@@ -121,7 +117,7 @@
 			})
 		}
 
-		$scope.removerAnexo = function(anexo){
+		$scope.removerAnexo = function (anexo) {
 			// $scope.$apply(function ($scope) {
 			// });
 			for (var i = 0; i < $scope.anexos.length; i++) {
@@ -134,7 +130,9 @@
 		$scope.removerEmpresa = function (cnpj) {
 			for (var i = 0; i < $scope.empresasPagadoras.length; i++) {
 				if ($scope.empresasPagadoras[i].numIns === cnpj) {
+					$scope.somaValoresRetencInss -= $scope.empresasPagadoras[i].valRetencInss;
 					$scope.empresasPagadoras.splice(i, 1);
+
 				}
 			}
 		}
@@ -199,6 +197,139 @@
 			}
 			// Colocar no útil
 		}
+
+		angular.element(document).ready(function () {
+			if (new Date() > getDataFimInc()) {
+				$scope.desabilitaEnvio = true;
+				alert("Prazo para envio da declaração encerrado em " + $scope.dadosRetencContribPrev.informacoesDatasPermitidas.fimInc);
+			}
+			else {
+				if (new Date() < getDataInicioInc()) {
+					$scope.desabilitaEnvio = true;
+					alert("Declaração de Retenção de contribuição previdenciária poderá ser enviada a partir do dia " + $scope.dadosRetencContribPrev.informacoesDatasPermitidas.iniInc);
+				}
+				else {
+					if ($scope.jaEnviadaHoje()) {
+						var txt;
+						var r = confirm("eclaração de retenção de contribuição previdenciária já foi enviada na data de hoje. Deseja Substituir?");
+						if (r == true) {
+							// Excluir declaração enviada.
+							//RN19 (Dúvida, tirar com Elayne)
+						} else {
+							$scope.desabilitaEnvio = true;
+						}
+					}
+				}
+			}
+		});
+
+
+		$scope.tratarInicioReferencia = function () {
+			var dataAtual = new Date();
+			if ($scope.verificaInicioReferenciaEhAnoMesAtual()) {
+				if (dataAtual.getDate() >
+					parseInt($scope.dadosRetencContribPrev.informacoesDatasPermitidas.diaLim)) {
+					$scope.desabilitaEnvio = true;
+				}
+			}
+			else {
+				$scope.desabilitaEnvio = true;
+			}
+			if (!$scope.verificaInicioReferenciaEhAnoAtual()) {
+				alert("Mês inicial de referência deverá ser dentro do ano corrente!");
+				$scope.mesInicialRef = "";
+			}
+			if (!$scope.verificaFimReferenciaEhAnoAtual()) {
+				alert("Mês final de referência deverá ser dentro do ano corrente!");
+				$scope.mesFinalRefRef = "";
+			}
+			if ($scope.verificaFimRefMaiorIniRef()) {
+				alert("Mês final de referência deverá ser posterior ou igual ao mês ano inicial de referência!");
+				$scope.mesFinalRefRef = "";
+			}
+
+		}
+
+		$scope.verificaFimRefMaiorIniRef = function () {
+			var splitDtFinRef = $scope.mesFinalRef.split("/");
+			var splitDtIniRef = $scope.mesInicialRef.split("/");
+			var anoIni = splitDtIniRef[1];
+			var anoFim = splitDtFinRef[1];
+			var mesIni = splitDtIniRef[1];
+			var mesFim = splitDtFinRef[1];
+			var dataIni = new Date(parseInt(anoIni), parseInt(mesIni));
+			var dataFim = new Date(parseInt(anoFim), parseInt(mesFim));
+			if (dataFim > dataIni) {
+				return true;
+			}
+			return false;
+		}
+
+		$scope.verificaFimReferenciaEhAnoAtual = function () {
+			var splitDtFinRef = $scope.mesFinalRef.split("/");
+			var ano = splitDtFinRef[1];
+			var dataAtual = new Date();
+			if (ano == dataAtual.getFullYear()) {
+				return true;
+			}
+			return false;
+		}
+
+		$scope.verificaInicioReferenciaEhAnoAtual = function () {
+			var splitDtIniRef = $scope.mesInicialRef.split("/");
+			var ano = splitDtIniRef[1];
+			var dataAtual = new Date();
+			if (ano == dataAtual.getFullYear()) {
+				return true;
+			}
+			return false;
+		}
+
+		$scope.verificaInicioReferenciaEhAnoMesAtual = function () {
+			var splitDtIniRef = $scope.mesInicialRef.split("/");
+			var ano = splitDtIniRef[1];
+			var mes = splitDtIniRef[0];
+			var dataAtual = new Date();
+			if (ano == dataAtual.getFullYear() && mes == dataAtual.getMonth()) {
+				return true;
+			}
+			return false;
+		}
+
+		$scope.getDataFimInc = function () {
+			var split = $scope.dadosRetencContribPrev.informacoesDatasPermitidas.fimInc.split("/");
+			var ano = split[2];
+			var mes = split[1];
+			var dia = split[0];
+			var dataLimite = new Date(parseInt(ano), parseInt(mes), parseInt(dia));
+			return dataLimite;
+		}
+
+		$scope.getDataInicioInc = function () {
+			var split = $scope.dadosRetencContribPrev.informacoesDatasPermitidas.iniInc.split("/");
+			var ano = split[2];
+			var mes = split[1];
+			var dia = split[0];
+			var dataLimite = new Date(parseInt(ano), parseInt(mes), parseInt(dia));
+			return dataLimite;
+		}
+
+		$scope.jaEnviadaHoje = function () {
+			for (var i = 0; i < $scope.dadosRetencContribPrev.declaracoesIncluidas_cmpIni_cmpFim.length; i++) {
+				var status = $scope.dadosRetencContribPrev.declaracoesIncluidas_cmpIni_cmpFim[i].staTus;
+				if (status == 1 || status == 2 || status == 3 || status == 4) {
+					var split = $scope.dadosRetencContribPrev.declaracoesIncluidas_cmpIni_cmpFim[i].datMov.split("/");
+					var ano = split[2];
+					var mes = split[1];
+					var dia = split[0];
+					var data = new Date(parseInt(ano), parseInt(mes), parseInt(dia));
+					if (data === new Date()) {
+						return true;
+					}
+				}
+			}
+			return false;
+		}
 	}
 
 
@@ -221,8 +352,4 @@
 		}
 		return contem;
 	}
-
-	// angular.element(document).ready(function() {
-	//   angular.bootstrap(document.getElementById("despesas_plano_de_saude"), ['app.cooperados.despesasPlanoDeSaude']);
-	// });
 })();
